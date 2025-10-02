b7ee32a9b113ad1ad290e88a2c89446b30f339b4
David <davidterreu@hotmail.com>
2025-10-02 09:34:53 +0200

    First commit

---
+++ ej1/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Ejercicio 1</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
+        $nombre = "David";
+        $edad = 40;
+        $ciudad = "Zaragoza"; ?>
+
+        <div class='frase'>Hola, me llamo <?php echo $nombre ?>, tengo  <?php echo $edad ?> años y soy de <?php echo $ciudad ?>.</div>
+
+        <div class='hora'>La hora de hoy es  <?php echo date("H:i:s"); ?> </div>
+
+        <div class='fecha'>La fecha de hoy es  <?php echo date("d-m-Y") ; ?> </div>
+
+    </body>
+</html>
