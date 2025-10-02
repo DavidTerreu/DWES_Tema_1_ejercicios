5c89c7df10be49148c4451bf2bba99bd739f4afa
David <davidterreu@hotmail.com>
2025-10-02 10:03:57 +0200

    Codigo header

--- ej2/header.php
+++ ej2/header.php
@@ -1,1 +1,10 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="about.php">Sobre mi</a></li>
+        <li><a href="services.php">Servicios</a></li>
+        <li><a href="contact.php">Contacto</a></li>
+    </ul>
+</nav>
+
 <?php
